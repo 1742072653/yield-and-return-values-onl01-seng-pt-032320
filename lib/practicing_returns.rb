fd41c67278716a32838428d587f011d60f93fbb8
Wenhao Guo <gwh1742072653@gmail.com>
2020-04-01 22:22:41 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def hello(array)
   i = 0
   collection = []
@@ -7,3 +8,16 @@ def hello(array)
   end
   collection
 end
+=======
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+>>>>>>> 609d48dc07f9fa77f0e93819ab289740b9a8a2ab
